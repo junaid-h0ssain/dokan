ca39dd75c743af1294192ab24a9c411db31100b3
Junaid <158271661+junaid-h0ssain@users.noreply.github.com>
2025-11-18 01:25:03 +0600

    DTO done

---
+++ src/main/java/com/dokan/dokan/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.dokan.dokan.dto;
+
+import com.dokan.dokan.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    
+    private String token;
+    private User user;
+}
